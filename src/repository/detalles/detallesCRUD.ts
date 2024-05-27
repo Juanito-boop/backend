8593903462978107c709d3b0ba096c8d5974c9e6
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-27 12:04:01 +0000

    chore: Remove unused CRUD files for detalles, productos, and usuarios

--- src/repository/detalles/detallesCRUD.ts
+++
@@ -1,4 +0,0 @@
-export const SQL_DETALLES = {
-  LISTAR: "SELECT * FROM detalles_facturas whre id_tienda = $1",
-  CREAR: "INSERT INTO detalles (id, nombre_producto, valor_producto_unitario, valor_producto_total, vendedor, fecha_venta, cantidad_producto, id_tienda) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
-}
