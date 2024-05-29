597cf76ae809e17b29fd6e6dc5f6fc24cfe056b2
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-29 08:57:27 +0000

    chore: Update server configuration to use portfinder for dynamic port assignment

--- src/config/api/Server.ts
+++ src/config/api/Server.ts
@@ -1,7 +1,8 @@
-import   cors      from "cors";
-import { config  } from "dotenv";
-import   express   from "express";
-import   morgan    from "morgan";
+import   cors         from "cors";
+import { config     } from "dotenv";
+import   express      from "express";
+import   morgan       from "morgan";
+import   portfinder   from 'portfinder';
 
 import seguridad       from "../../middleware/Seguridad";
 import rutasCategorias from "../../routes/rutasCategorias";
@@ -34,20 +35,29 @@ class Servidor {
     }
 
     public activarRutas(): void {
+        this.app.get("/", (_req, res) => {res.send("hello world")});
         this.app.use(`${this.v1}/categorias`, seguridad.revisar, rutasCategorias);// listo
         this.app.use(`${this.v1}/detalles`, seguridad.revisar, rutasDetalles);
         this.app.use(`${this.v1}/facturas`, seguridad.revisar, rutasFacturas);// listo
         this.app.use(`${this.v1}/productos`, seguridad.revisar, rutasProductos);// listo
         this.app.use(`${this.v1}/tiendas`, seguridad.revisar, rutasTienda);// listo
         this.app.use(`${this.v1}/token`, tokenRuta);// listo
-        this.app.use(`${this.v1}/usuarios`, seguridad.revisar, rutasUsuario);
+        this.app.use(`${this.v1}/usuarios`, seguridad.revisar, rutasUsuario);// listo
     }    
     
     public arrancar(): void {
         const puerto = this.app.get("PORT");
-        this.app.listen(puerto, () => {
-            console.log("Servidor corriendo en " + puerto);
-        });
+
+        portfinder.basePort = parseInt(puerto, 10);  // Establece el puerto base para portfinder
+        portfinder.getPortPromise()
+            .then((port) => {
+                this.app.listen(port, () => {
+                    console.log(`Servidor corriendo en el puerto ${port}`);
+                });
+            })
+            .catch((err) => {
+                console.error("No se pudo encontrar un puerto disponible:", err);
+            });
     }
 }
 
