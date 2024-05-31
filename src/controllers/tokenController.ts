ab8a86aef29d72b910c3099e400b4089ba505625
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-31 05:17:41 +0000

    feat: Refactor tokenController and DetallesFacturaController for consistent formatting and error handling

--- src/controllers/tokenController.ts
+++ src/controllers/tokenController.ts
@@ -3,18 +3,18 @@ import { Token } from "../interface/interfaces";
 import tokenDAO from "../dao/tokenDAO";
 
 class tokenController {
-  public async createToken(req: Request, res: Response): Promise<void> {
-    const { username, password } = req.body;
-    const data: Token[] = [username, password];
+	public async createToken(req: Request, res: Response): Promise<void> {
+		const { username, password } = req.body;
+		const data: Token[] = [username, password];
 
-    const result = await tokenDAO.generateToken(data);
+		const result = await tokenDAO.generateToken(data);
 
-    if (result.isSuccess) {
-      res.status(200).json(result.getValue())
-    } else {
-      res.status(400).json({ respuesta: result.errorValue() });
-    }
-  }
+		if (result.isSuccess) {
+			res.status(200).json(result.getValue())
+		} else {
+			res.status(400).json({ respuesta: result.errorValue() });
+		}
+	}
 }
 
 const TokenController = new tokenController();
