9228b66a32b6f7d75a91ddf483c1233dacc6d21d
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-31 05:18:05 +0000

    feat: Add cache middleware for improved performance

--- src/middleware/Seguridad.ts
+++ src/middleware/Seguridad.ts
@@ -5,34 +5,34 @@ import { config } from "dotenv";
 config({ path: "./.env" });
 
 class Seguridad {
-  public revisar(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+	public revisar(req: Request, res: Response, next: NextFunction) {
+		const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-      return res.status(401).json({ Respuesta: "Falta el token de autorización" });
-    }
+		if (!authHeader) {
+			return res.status(401).json({ Respuesta: "Falta el token de autorización" });
+		}
 
-    const tokenParts = authHeader.split(" ");
-    if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
-      return res.status(401).json({ Respuesta: "Formato del token incorrecto" });
-    }
+		const tokenParts = authHeader.split(" ");
+		if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+			return res.status(401).json({ Respuesta: "Formato del token incorrecto" });
+		}
 
-    const token = tokenParts[1];
-    try {
-      const secretKey = process.env.JWT_SECRET_KEY || 'LaSuperClave';
-      const decoded = jwt.verify(token, secretKey);
-      (req as any).user = decoded; // Attach decoded token to the request object
-      next();
-    } catch (error) {
-      if (error instanceof jwt.TokenExpiredError) {
-        return res.status(401).json({ Respuesta: "El token ha expirado" });
-      } else if (error instanceof jwt.JsonWebTokenError) {
-        return res.status(401).json({ Respuesta: "El token no es válido" });
-      } else {
-        return res.status(500).json({ Respuesta: "Error al procesar el token" });
-      }
-    }
-  }
+		const token = tokenParts[1];
+		try {
+			const secretKey = process.env.JWT_SECRET_KEY || 'LaSuperClave';
+			const decoded = jwt.verify(token, secretKey);
+			(req as any).user = decoded; // Attach decoded token to the request object
+			next();
+		} catch (error) {
+			if (error instanceof jwt.TokenExpiredError) {
+				return res.status(401).json({ Respuesta: "El token ha expirado" });
+			} else if (error instanceof jwt.JsonWebTokenError) {
+				return res.status(401).json({ Respuesta: "El token no es válido" });
+			} else {
+				return res.status(500).json({ Respuesta: "Error al procesar el token" });
+			}
+		}
+	}
 }
 
 const seguridad = new Seguridad();
