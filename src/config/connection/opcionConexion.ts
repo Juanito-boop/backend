3bf2d47e5fd9580c6a08357dd7e9f52e1090ff6b
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-31 05:17:24 +0000

    chore: Update connection files to use consistent indentation and formatting

--- src/config/connection/opcionConexion.ts
+++ src/config/connection/opcionConexion.ts
@@ -2,7 +2,7 @@ import pgPromise from "pg-promise";
 import { camelizeColumns, IClient } from "./funcionConexion";
 
 export const opcionesPG: pgPromise.IInitOptions<IClient> = {
-    receive(data){
-        camelizeColumns(data);
-    },
+	receive(data){
+		camelizeColumns(data);
+	},
 };
