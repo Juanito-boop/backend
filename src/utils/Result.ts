3ff90f226fb2f2364855e4109170646a03bd9924
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-05-31 05:18:15 +0000

    chore: Add Cache.ts utility for caching functionality

--- src/utils/Result.ts
+++ src/utils/Result.ts
@@ -1,42 +1,42 @@
 class Result<T, E = string> {
-  public isSuccess: boolean;
-  public isFailure: boolean;
-  public error: E | null;
-  private value: T | null;
+	public isSuccess: boolean;
+	public isFailure: boolean;
+	public error: E | null;
+	private value: T | null;
 
-  private constructor(isSuccess: boolean, error?: E, value?: T) {
-    if (isSuccess && error) {
-      throw new Error("InvalidOperation: A result cannot be successful and contain an error");
-    }
-    if (!isSuccess && !error) {
-      throw new Error("InvalidOperation: A failing result needs to contain an error message");
-    }
+	private constructor(isSuccess: boolean, error?: E, value?: T) {
+		if (isSuccess && error) {
+			throw new Error("InvalidOperation: A result cannot be successful and contain an error");
+		}
+		if (!isSuccess && !error) {
+			throw new Error("InvalidOperation: A failing result needs to contain an error message");
+		}
 
-    this.isSuccess = isSuccess;
-    this.isFailure = !isSuccess;
-    this.error = error || null;
-    this.value = value || null;
-  }
+		this.isSuccess = isSuccess;
+		this.isFailure = !isSuccess;
+		this.error = error || null;
+		this.value = value || null;
+	}
 
-  public getValue(): T {
-    if (!this.isSuccess) {
-      throw new Error("Can't get the value of an error result. Use 'errorValue' instead.");
-    }
+	public getValue(): T {
+		if (!this.isSuccess) {
+			throw new Error("Can't get the value of an error result. Use 'errorValue' instead.");
+		}
 
-    return this.value as T;
-  }
+		return this.value as T;
+	}
 
-  public errorValue(): E {
-    return this.error as E;
-  }
+	public errorValue(): E {
+		return this.error as E;
+	}
 
-  public static success<U>(value?: U): Result<U> {
-    return new Result<U>(true, undefined, value);
-  }
+	public static success<U>(value?: U): Result<U> {
+		return new Result<U>(true, undefined, value);
+	}
 
-  public static fail<U, F>(error: F): Result<U, F> {
-    return new Result<U, F>(false, error);
-  }
+	public static fail<U, F>(error: F): Result<U, F> {
+		return new Result<U, F>(false, error);
+	}
 }
 
 export default Result;
